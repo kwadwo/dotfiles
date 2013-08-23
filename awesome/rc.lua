73bb95f0c43683038cbc08c84f2b1769f824cb78
Drew DeVault <sir@cmpwn.com>
2013-08-22 21:15:59 -0600

    Remove vicious from awesome config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -5,7 +5,6 @@ awful.rules = require("awful.rules")
 require("awful.autofocus")
 -- Widget and layout library
 local wibox = require("wibox")
-local vicious = require("vicious")
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
