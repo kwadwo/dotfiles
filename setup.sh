4792b052252849b5414d08e3e3f4e4d52fb74ad2
Drew DeVault <sir@cmpwn.com>
2013-09-17 18:12:02 -0600

    Improve layout of files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 USER=$(whoami)
 HOME=/home/$USER
+# Make configuration files
 mkdir -p $HOME/.vim $HOME/.i3 $HOME/.config/moc /$HOME/.config/i3status
-ln -s vim/.vim $HOME/.vim
-ln -s vim/.vimrc $HOME/.vimrc
-ln -s bash/.bashrc $HOME/.bashrc
-ln -s x/.xinitrc $HOME/.xinitrc
+ln -s vim/vim $HOME/.vim
+ln -s vim/vimrc $HOME/.vimrc
+ln -s bash/bashrc $HOME/.bashrc
+ln -s x/xinitrc $HOME/.xinitrc
 ln -s i3/config $HOME/.i3/config
-ln -s i3status $HOME/.config/i3status/config
+ln -s i3/i3status $HOME/.config/i3status/config
