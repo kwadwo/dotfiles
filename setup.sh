9bb1bb828bcb1a2bdd97e417a473dc29fbc1731a
Drew DeVault <sir@cmpwn.com>
2013-09-17 18:09:31 -0600

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-mkdir -p ~/.vim ~/.i3
-ln -s vim/.vim ~/.vim
-ln -s vim/.vimrc ~/.vimrc
-ln -s bash/.bashrc ~/.bashrc
-ln -s x/.xinitrc ~/.xinitrc
-ln -s i3/config ~/.i3/config
-ln -s i3status ~/.config/i3status/config
+USER=$(whoami)
+HOME=/home/$USER
+mkdir -p $HOME/.vim $HOME/.i3 $HOME/.config/moc /$HOME/.config/i3status
+ln -s vim/.vim $HOME/.vim
+ln -s vim/.vimrc $HOME/.vimrc
+ln -s bash/.bashrc $HOME/.bashrc
+ln -s x/.xinitrc $HOME/.xinitrc
+ln -s i3/config $HOME/.i3/config
+ln -s i3status $HOME/.config/i3status/config
