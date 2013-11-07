fb37155e642c9b2cff36ac986695537646d91048
Drew DeVault <sir@cmpwn.com>
2013-11-07 01:30:07 -0800

    Random bash quote must have score >0

--- fish/config.fish
+++ fish/config.fish
@@ -38,4 +38,4 @@ function fish_prompt --description 'Write out the prompt'
 
     end
 end
-psql -At -d bash -c "SELECT text FROM quotes OFFSET random()*(SELECT COUNT(*) FROM quotes) LIMIT 1" | fold -s
+psql -At -d bash -c "SELECT text FROM quotes WHERE score > 0 OFFSET random()*(SELECT COUNT(*) FROM quotes WHERE score > 0) LIMIT 1" | fold -s
